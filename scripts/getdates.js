8acb932146b1dcf7d775115bdf730729bb00502f
Emmaowilli <145481492+Emmaowilli@users.noreply.github.com>
2025-03-04 21:18:57 +0300

    seond commit

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// getdates.js
+
+// Set the current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set the last modified date in the footer
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
