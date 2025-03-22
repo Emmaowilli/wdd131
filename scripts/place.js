7897bb08641bb9d7cdc7272a022c0d759bdafe1c
Emmaowilli <145481492+Emmaowilli@users.noreply.github.com>
2025-03-22 19:42:52 +0300

    commit

--- scripts/place.js
+++ scripts/place.js
@@ -1,13 +1,10 @@
-// Footer year and last modified
-document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// Wind chill calculation
 const temp = parseFloat(document.getElementById("temp").textContent);
 const speed = parseFloat(document.getElementById("speed").textContent);
 
 function calculateWindChill(t, s) {
-  // Wind chill formula for Celsius
   return 13.12 + 0.6215 * t - 11.37 * Math.pow(s, 0.16) + 0.3965 * t * Math.pow(s, 0.16);
 }
 
